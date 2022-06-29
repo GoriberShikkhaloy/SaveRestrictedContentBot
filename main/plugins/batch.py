609c6cd559559e0fb1ea5f9996c09f356ef2ee25
GoriberShikkhaloy <101473934+GoriberShikkhaloy@users.noreply.github.com>
2022-06-29 19:29:51 +0600

    Update batch.py

--- main/plugins/batch.py
+++ main/plugins/batch.py
@@ -22,7 +22,7 @@ from pyrogram.errors import FloodWait
 from ethon.pyfunc import video_metadata
 from ethon.telefunc import force_sub
 
-ft = f"To use this bot you've to join @{fs}."
+ft = f"এই বটটি ব্যবহার করার জন্য আপনাকে হুদাই @{fs} এই চ্যানেলে জয়েন হতে হবে।"
 
 batch = []
 
@@ -41,31 +41,31 @@ async def _batch(event):
         await event.reply(r)
         return       
     if f'{event.sender_id}' in batch:
-        return await event.reply("You've already started one batch, wait for it to complete you dumbfuck owner!")
+        return await event.reply("আপনি ইতিমধ্যেই একটি ব্যাচ ডাউনলোড শুরু করেছেন। এটি শেষ না হওয়া পর্যন্ত অপেক্ষা করুন!")
     async with Drone.conversation(event.chat_id) as conv: 
         if s != True:
-            await conv.send_message("Send me the message link you want to start saving from, as a reply to this message.", buttons=Button.force_reply())
+            await conv.send_message("আপনি যে মেসেজ থেকে ব্যাচ ডাউনলোড করতে চাচ্ছেন সেটির লিংক আমাকে এই মেসেজের রিপ্লাই হিসেবে দিন।", buttons=Button.force_reply())
             try:
                 link = await conv.get_reply()
                 try:
                     _link = get_link(link.text)
                 except Exception:
-                    await conv.send_message("No link found.")
+                    await conv.send_message("কোনো লিংক পাওয়া যায় নি!")
             except Exception as e:
                 print(e)
-                return await conv.send_message("Cannot wait more longer for your response!")
-            await conv.send_message("Send me the number of files/range you want to save from the given message, as a reply to this message.", buttons=Button.force_reply())
+                return await conv.send_message("আপনার রেসপন্সের জন্য আর বেশি অপেক্ষা করতে পারছি না!")
+            await conv.send_message("মাত্রই আমাকে যে লিংক দিলেন, ওইটা থেকে শুরু করে আপনি কতটি কনটেন্ট ডাউনলোড করতে চান তা এই মেসেজের রিপ্লাই হিসেবে আমাকে ইংরেজিতে লিখুন !", buttons=Button.force_reply())
             try:
                 _range = await conv.get_reply()
             except Exception as e:
                 print(e)
-                return await conv.send_message("Cannot wait more longer for your response!")
+                return await conv.send_message("আপনার রেসপন্সের জন্য আর বেশি অপেক্ষা করতে পারছি না!")
             try:
                 value = int(_range.text)
-                if value > 100:
-                    return await conv.send_message("You can only get upto 100 files in a single batch.")
+                if value > 200:
+                    return await conv.send_message("প্রতিবার ব্যাচ ডাউনলোডে আপনি সর্বোচ্চ ২০০ টি ফাইল ডাউনলোড করতে পারবেন।")
             except ValueError:
-                return await conv.send_message("Range must be an integer!")
+                return await conv.send_message("রেঞ্জটি অবশ্যই বাস্তব ধনাত্মক সংখ্যা হতে হবে!")
             s, r = await check(userbot, Bot, _link)
             if s != True:
                 await conv.send_message(r)
@@ -80,11 +80,11 @@ async def run_batch(userbot, client, sender, link, _range):
     for i in range(_range):
         timer = 60
         if i < 25:
-            timer = 5
+            timer = 8
         if i < 50 and i > 25:
-            timer = 10
-        if i < 100 and i > 50:
-            timer = 15
+            timer = 20
+        if i < 200 and i > 50:
+            timer = 35
         if not 't.me/c/' in link:
             if i < 25:
                 timer = 2
@@ -93,9 +93,9 @@ async def run_batch(userbot, client, sender, link, _range):
         try:
             await get_bulk_msg(userbot, client, sender, link, i) 
         except FloodWait as fw:
-            await asyncio.sleep(fw.seconds + 5)
+            await asyncio.sleep(fw.seconds + 8)
             await get_bulk_msg(userbot, client, sender, link, i)
-        protection = await client.send_message(sender, f"Sleeping for `{timer}` seconds to avoid Floodwaits and Protect account!")
+        protection = await client.send_message(sender, f"আপনার অ্যাকাউন্টের নিরাপত্তা ও স্প্যাম হিসেবে শনাক্ত না হবার জন্য `{timer}` সেকেন্ড কার্যক্রম বন্ধ রাখা হচ্ছে!")
         time.sleep(timer)
         await protection.delete()
             
